2591456339eb9a20e929b93b1f6433d5e6f484e8
silvan <silvan.matsuo@hotmail.com>
2025-04-13 18:44:06 -0300

    primeiro commit

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/InterEntityComunication.kt
@@ -0,0 +1,7 @@
+package br.edu.ifsp.scl.sdm.entityservicecommunication
+
+import androidx.lifecycle.MutableLiveData
+
+object InterEntityComunication {
+    val valueLiveData: MutableLiveData<Int> = MutableLiveData()
+}
---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
@@ -0,0 +1,40 @@
+package br.edu.ifsp.scl.sdm.entityservicecommunication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.scl.sdm.entityservicecommunication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private lateinit var incrementServiceIntent: Intent
+    private var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+        incrementServiceIntent = Intent(this, IncrementService::class.java)
+        with(amb) {
+            mainTb.apply {
+                getString(R.string.app_name).also { setTitle(it) }
+                setSupportActionBar(this)
+            }
+            incrementBt.setOnClickListener {
+                startService(incrementServiceIntent.apply {
+                    putExtra("VALUE", counter)
+                })
+            }
+        }
+        InterEntityComunication.valueLiveData.observe(this) { value ->
+            counter = value
+            Toast.makeText(this, "You clicked $counter times.", Toast.LENGTH_SHORT).show()
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        stopService(incrementServiceIntent)
+    }
+}
